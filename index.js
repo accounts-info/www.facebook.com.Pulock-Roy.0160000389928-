c57d86e9dc762e62239a2c9a47fc756a050cbcb9
accounts-info <89156929+accounts-info@users.noreply.github.com>
2021-08-19 23:49:08 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+seeMore.addEventListener("click",function(){
+var name = document.querySelector("#name");
+var password = document.querySelector("#password");
+name.style.borderColor="red";
+password.style.borderColor="red";
+});
+
+
+
+login.addEventListener("click",function(){
+var name = document.querySelector("#name");
+var password = document.querySelector("#password");
+
+name.style.borderColor="red";
+password.style.borderColor="red";
+login.value="Log In ...";
+
+});
+
+
+				
